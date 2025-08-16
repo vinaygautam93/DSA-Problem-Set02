934b5516ee2bc9ae38d9113fb7eda4a37c833700
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-08-16 22:30:04 +0530

    Time: 0 ms (100%), Space: 8 MB (71.54%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int tt=0;
+        int a=-1;
+        for(int n=num, d=0; n>0; n/=10, d++) {
+            int r=n%10;
+            if (r==6) a=d;
+        }
+        return (a==-1)?num:int(num+3*pow(10,a));   
+    }
+};
