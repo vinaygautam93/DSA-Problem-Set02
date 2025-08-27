bee4dd64e4dae09cc70ca864bee2cd565308d7c7
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-08-27 23:47:52 +0530

    Time: 52 ms (15.11%), Space: 75.1 MB (54.1%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -4,7 +4,6 @@ public:
 
 
         unordered_map<int, int> mpp;
-
         int i = 0, j = 0, res = 0;
 
         int v; 
@@ -16,15 +15,8 @@ public:
         {
 
             mpp[fruits[j]]++;
-
-            
-
             if(mpp.size() <= 2) res = max(res, j-i+1);
-
-            
-
             else
-
             {
 
                 mpp[fruits[i]]--;
