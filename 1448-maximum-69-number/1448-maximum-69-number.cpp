a0317dd5d9dd15d20b5b42c7502dc06d9640e2fb
Vinay Gautam <121504999+vinaygautam93@users.noreply.github.com>
2025-08-27 10:40:24 +0530

    Time: 0 ms (100%), Space: 8.1 MB (10.68%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
-        
-        vector< int > vg;
-       
-        while( num > 0 ) {
-            int re = num % 10;
-            vg.push_back(re);
-            num = num /10;
-        }
-
-        reverse(vg.begin() , vg.end());
-
-        int n = vg.size();
-
-        for( int i = 0; i < n; i ++ ) {
-
-            if( vg[i] == 6 ) {
-                vg[i] = 9;
+    int maximum69Number(int num) {
+        string s = to_string(num); 
+        for (char &c : s) {
+            if (c == '6') { 
+                c = '9';
                 break;
             }
         }
-
-        int ans = 0;
-
-        for( int i = 0; i < n; i ++ ) {
-            
-            ans = ans * 10 + vg[i];
-
-        }
-        return ans;
-
-
+        return stoi(s); 
     }
 };
